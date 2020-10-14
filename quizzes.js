29b2f290fe9ed397c4123823ce568a3b2703233d
= <awesomepranshu@gmail.com>
2020-10-15 02:03:17 +0530

    Add funcionality to build from json file

---
+++ quizzes.js
@@ -0,0 +1,18 @@
+quizzes = {
+    "Political Quiz": {
+        "Event date": "12-08-2020",
+        "Event time": "8 pm",
+        "Event description": "A quiz about Indian an world politics",
+        "Allowed for": "IIT Mandi Students",
+        "Event Poster": "/posters/political.jpg",
+        "Registration Link": "https://forms.google.com/politicalquiz"
+    },
+    "Jeopardy": {
+        "Event date": "14-08-2020",
+        "Event time": "7 pm",
+        "Event description": "Business quiz having questions in four categories viz Tech, Finance, History, Startup Stories, Business Model",
+        "Allowed for": "IIT Mandi Students",
+        "Event Poster": "/posters/jeopardy.jpg",
+        "Registration Link": "https://forms.google.com/jeopardy"
+    }
+}

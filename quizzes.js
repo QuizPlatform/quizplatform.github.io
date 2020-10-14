7790258b3452a5989817e81647f8bb967b425a2e
= <awesomepranshu@gmail.com>
2020-10-15 02:14:27 +0530

    Fix bug and test code

--- quizzes.js
+++ quizzes.js
@@ -5,7 +5,8 @@ quizzes = {
         "Event description": "A quiz about Indian an world politics",
         "Allowed for": "IIT Mandi Students",
         "Event Poster": "/posters/political.jpg",
-        "Registration Link": "https://forms.google.com/politicalquiz"
+        "Registration Link": "https://forms.google.com/politicalquiz",
+        "Event Link" : "politicalquiz"
     },
     "Jeopardy": {
         "Event date": "14-08-2020",
@@ -13,6 +14,16 @@ quizzes = {
         "Event description": "Business quiz having questions in four categories viz Tech, Finance, History, Startup Stories, Business Model",
         "Allowed for": "IIT Mandi Students",
         "Event Poster": "/posters/jeopardy.jpg",
-        "Registration Link": "https://forms.google.com/jeopardy"
+        "Registration Link": "https://forms.google.com/jeopardy",
+        "Event Link" : "jeopardy"
+    },
+    "India Quiz": {
+        "Event date": "21-02-2020",
+        "Event time": "8:30 pm",
+        "Event description": "The quizzing club,Qurosity is back with the first Quiz in this semester, although it is not the fearful one where good marks are scarce! The *INDIA QUIZ* will be conducted on 21st Feb,2020 which will start right from 8:30 PM,so have your dinner early and show us how much you know about our country!",
+        "Allowed for": "IIT Mandi Students",
+        "Event Poster": "/posters/indiaquiz.jpg",
+        "Registration Link": "https://forms.google.com/indiaquiz",
+        "Event Link" : "indiaquiz"
     }
 }
